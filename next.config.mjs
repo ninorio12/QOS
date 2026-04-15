cc9a8bd35976037f4a91a5090d009526270ccda3
Thomas <thomalves12@gmail.com>
2026-04-15 14:28:57 +0200

    fix: webpack polling pour file watching Windows

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withBundleAnalyzer = bundleAnalyzer({
 })
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 200,
+      }
+    }
+    return config
+  },
+}
 
 export default withBundleAnalyzer(nextConfig)
